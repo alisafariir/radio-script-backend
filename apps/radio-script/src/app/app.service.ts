f33c209f2134391129e2ce68a9a40a220a2974a3
Ali Safari <asafari72@gmail.com>
2025-04-14 10:53:38 +0330

    debug envs

--- apps/radio-script/src/app/app.service.ts
+++ apps/radio-script/src/app/app.service.ts
@@ -6,6 +6,11 @@ export class AppService {
   constructor(private readonly configService: ConfigService) {}
 
   getData(): { message: string } {
+    console.log(
+      this.configService.get<number>("SMTP_HOST"),
+      this.configService.get<number>("SMTP_PORT")
+    );
+    console.log("ENVS:", process.env);
     return { message: `${this.configService.get<string>("APP_NAME")} 👋` };
   }
 }

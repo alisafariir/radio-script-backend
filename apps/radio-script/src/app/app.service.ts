1b057eb9aece50a700ee2f26080318542e26e62a
Ali Safari <asafari72@gmail.com>
2025-04-14 10:09:01 +0330

    change message

--- apps/radio-script/src/app/app.service.ts
+++ apps/radio-script/src/app/app.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class AppService {
   constructor(private readonly configService: ConfigService) {}
 
   getData(): { message: string } {
-    return { message: `The ${this.configService.get<string>('APP_NAME')} say hello. 👋` };
+    return { message: `${this.configService.get<string>("APP_NAME")} 👋` };
   }
 }

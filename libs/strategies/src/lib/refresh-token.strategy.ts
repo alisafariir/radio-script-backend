52d86f53b2e4f9ade55ce17f8b097fd0fff19b14
Ali <you@example.com>
2025-04-17 16:11:17 +0330

    fix: Improve refresh token extraction logic from request cookies and headers

--- libs/strategies/src/lib/refresh-token.strategy.ts
+++ libs/strategies/src/lib/refresh-token.strategy.ts
@@ -11,7 +11,12 @@ export class RefreshTokenStrategy extends PassportStrategy(Strategy, 'refresh-to
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
         (request: Request) => {
-          return request?.cookies?.refresh_token;
+          // First try to get from cookies
+          if (request?.cookies?.refresh_token) {
+            return request.cookies.refresh_token;
+          }
+          // Then try to get from header (for mobile applicaton or unsupported cookies)
+          return request?.headers?.['refresh-token'] as string;
         },
       ]),
       secretOrKey: configService.getOrThrow<string>('JWT_SECRET_KEY', ''),
@@ -20,7 +25,7 @@ export class RefreshTokenStrategy extends PassportStrategy(Strategy, 'refresh-to
   }
 
   async validate(req: Request, payload: any) {
-    const refresh_token = req.cookies?.refresh_token;
+    const refresh_token = req.cookies?.refresh_token || req.headers?.['refresh-token'];
 
     return { ...payload, refresh_token };
   }

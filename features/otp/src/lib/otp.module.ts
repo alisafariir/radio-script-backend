3ef6ba570a468404ae79146a0f7cda78943c487d
Ali <you@example.com>
2025-04-18 04:06:12 +0330

    feat: integrate I18nModule into Auth and Otp modules for internationalization support

--- features/otp/src/lib/otp.module.ts
+++ features/otp/src/lib/otp.module.ts
@@ -1,14 +1,17 @@
 import { Otp } from '@/entities';
 import { EncryptionService } from '@/helpers';
+import { MailService } from '@/mail';
+import { SmsService } from '@/sms';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { I18nModule } from 'nestjs-i18n';
 import { OtpController } from './otp.controller';
 import { OtpService } from './otp.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Otp])],
   controllers: [OtpController],
-  providers: [OtpService, EncryptionService],
+  providers: [OtpService, EncryptionService, MailService, SmsService, I18nModule],
   exports: [OtpService],
 })
 export class OtpModule {}
